29b4129492cd9cc679d01e2025bd8be063aa9d50
Martin Lemesle <lemesle.martin@hotmail.fr>
2023-08-01 08:46:49 +0200

    Log endpoint uses previous result

--- src/layers/log.rs
+++ src/layers/log.rs
@@ -1,5 +1,8 @@
+use std::{future::Future, pin::Pin};
+
 use axum::http::Request;
 use chrono::Utc;
+use colored::Colorize;
 use tower::{Layer, Service};
 
 #[derive(Clone)]
@@ -15,13 +18,15 @@ impl<T> Log<T> {
 
 impl<T, ReqBody> Service<Request<ReqBody>> for Log<T>
 where
-    T: Service<Request<ReqBody>>,
+    T: Service<Request<ReqBody>> + Send + Clone + 'static,
+    <T as Service<Request<ReqBody>>>::Future: Send + 'static,
+    ReqBody: Send + 'static,
 {
     type Response = T::Response;
 
     type Error = T::Error;
 
-    type Future = T::Future;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn poll_ready(
         &mut self,
@@ -31,20 +36,36 @@ where
     }
 
     fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
-        println!(
-            "[{}] {} -> {}",
-            Utc::now().to_rfc3339(),
-            req.method(),
-            req.uri()
-        );
-        self.inner.call(req)
+        let req_method = req.method().to_string();
+        let req_uri = req.uri().to_string();
+        let mut inner = self.inner.clone();
+
+        Box::pin(async move {
+            let res = inner.call(req).await;
+            let status = if res.is_ok() {
+                "SUCCESS".green()
+            } else {
+                "ERROR".red()
+            };
+            println!(
+                "[{}] {} {} -> {}",
+                Utc::now().to_rfc3339().underline(),
+                status,
+                req_method.yellow(),
+                req_uri.bright_blue(),
+            );
+            res
+        })
     }
 }
 
 #[derive(Clone)]
 pub struct LogLayer;
 
-impl<S> Layer<S> for LogLayer {
+impl<S> Layer<S> for LogLayer
+where
+    S: Send + 'static,
+{
     type Service = Log<S>;
 
     fn layer(&self, inner: S) -> Self::Service {
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use axum::{routing::get, Router};
+use axum::{extract::Query, http::StatusCode, routing::get, Router};
 use layers::log::LogLayer;
+use serde::Deserialize;
+use tower::ServiceBuilder;
 
 mod layers;
 
+#[derive(Deserialize)]
+struct Q {
+    success: bool,
+}
+
+async fn do_it(Query(q): Query<Q>) -> Result<(), StatusCode> {
+    if q.success {
+        Ok(())
+    } else {
+        Err(StatusCode::NOT_ACCEPTABLE)
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .layer(LogLayer);
+    let app = Router::new().route("/", get(do_it)).layer(LogLayer);
+    // let app = ServiceBuilder::new().layer(LogLayer).service(app);
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
